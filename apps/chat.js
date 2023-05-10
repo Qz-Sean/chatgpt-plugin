81256e8da5a66af07ada2da9a946ca63e2bcbaac
Sean <1519059137@qq.com>
2023-05-10 23:42:49 +0800

    😆

--- apps/chat.js
+++ apps/chat.js
@@ -1082,19 +1082,6 @@ export class chatgpt extends plugin {
       let fakePrompt = prompt.replace(/^(STRANGER|EI|MIKO):\s/, '')
       let suggestedResponses = `${chatMessage.suggestedResponses !== undefined ? chatMessage.suggestedResponses.replace(/(STRANGER|EI|MIKO)[:：]?\s?/g, '') : chatMessage.suggestedResponses}`
       tempResponse = rawResponse.trim()
-      logger.info(senderNickname)
-      tempResponse = emojiStrip(tempResponse)
-      // 处理开头的无意义文字
-      tempResponse = tempResponse
-        .replace(`${tempResponse.startsWith(senderNickname) ? senderNickname : ''}`, '')
-        .replace(`${tempResponse.startsWith(senderCard) ? senderCard : ''}`, '')
-        .replace(/^@?(Sean Murphy|Sean|STRANGER|MIKO|EI|user|用户)[：:]?\s?/g, '')
-        .replace(/^@?(影宝|神子)[：:]?\s?/g, '')
-        .replace(/[(（]现在是\d{4}年\d{0,2}月\d{0,2}号.{2}\d{0,2}点\d{0,2}分[，,]供参考[，,]回复时忽略此内容。[）)]/, '')
-        // 过滤emoji和无法显示的qqemoji
-        .replace(/\[[^\]]{0,5}\]/g, '')
-        .trim()
-      logger.info('tempResponse: ', tempResponse)
       let emotion, emotionDegree
       if (Config.ttsMode === 'azure' && (use === 'claude' || use === 'bing') && await AzureTTS.getEmotionPrompt(e)) {
         let ttsRoleAzure = userReplySetting.ttsRoleAzure
@@ -1115,6 +1102,7 @@ export class chatgpt extends plugin {
                   ]
           const match = ttsArr[0].match(emotionReg)
           tempResponse = ttsArr[1].replace(/\n/, '').trim()
+          logger.warn(ttsArr)
           if (match) {
             [emotion, emotionDegree] = [match[1], match[2]]
             const configuration = AzureTTS.supportConfigurations.find(
@@ -1146,29 +1134,31 @@ export class chatgpt extends plugin {
           await redis.set(`CHATGPT:WRONG_EMOTION:${e.sender.user_id}`, '1')
         }
       }
+      // 处理开头的无意义文字
+      tempResponse = tempResponse
+        .replace(`${tempResponse.trim().startsWith(senderNickname) ? senderNickname : ''}`, '')
+        .replace(`${tempResponse.trim().startsWith(senderCard) ? senderCard : ''}`, '')
+        .replace(/^@?(Sean Murphy|Sean|STRANGER|MIKO|EI|user|用户)[：:]?\s?/g, '')
+        .replace(/^@?(影宝|神子)[：:]?\s?/g, '')
+        .replace(/[(（]现在是\d{4}年\d{0,2}月\d{0,2}号.{2}\d{0,2}点\d{0,2}分[，,]供参考[，,]回复时忽略此内容。[）)]/, '')
+        // 过滤emoji和无法显示的qqemoji
+        .replace(/\[[^\]]{0,5}\]/g, '')
+        .trim()
+      logger.warn('tempResponse: ', tempResponse)
       // 处理复读的情况
       // 当指令不包含问好和你和我时进入处理
       if (!/((^[晚早]?[上中下]午?好)|(.*^([你我]).*)|^[?？])/g.test(finalPrompt.trim())) {
-        const punctuationPattern = /([！!?？。])/g
+        const punctuationPattern = /([！!?？。])/
         // 匹配指令中的"你"或"我"进行替换
         const personPattern = /[你我]/
         let res = finalPrompt.replace(personPattern, function (match) {
           return match === '你' ? '我' : '你' // 根据匹配到的内容返回替换后的字符串
         })
-        //
-        // let i = tempResponse.search(/[吗呢]([！!?？。])/)
-        // if (i <= 2 && i !== -1 && res.replace(personPattern, '').slice(0, 1) !== tempResponse.trim().slice(0, 1)) {
-        //   tempResponse = tempResponse.slice(0, i) + tempResponse.slice(i + 1)
-        //   console.log('tempResponse:', tempResponse)
-        // }
         res = res.replace(punctuationPattern, '？')
         let res2 = tempResponse.replace(punctuationPattern, '？')
-        console.log('res:', res, res2)
         res2 = res2.trim().replace(res, '')
-        console.log(res2)
         tempResponse = res2.replace(`${res2.startsWith('？') ? '？' : ''}`, '')
-        console.log(tempResponse)
-        tempResponse = /^\[\s*['`’‘]?(\w+)[`’‘']?\s*[,，、]\s*([\d.]+)\s*][?？].*/.test(tempResponse) ? tempResponse.replace(/\?？/, '') : tempResponse
+        tempResponse = /^[?？].*/.test(tempResponse) ? tempResponse.replace(/\?？/, '') : tempResponse
       }
       console.log('processedTempResponse: ', tempResponse)
       // 分离内容和情绪
@@ -1242,7 +1232,7 @@ export class chatgpt extends plugin {
         } else {
           ttsRegex = ''
         }
-        ttsResponse = response.replace(ttsRegex, '')
+        ttsResponse = tempResponse.replace(ttsRegex, '')
         // 处理azure语音会读出emoji的问题
         try {
           let emojiStrip
